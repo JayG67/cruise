91ecc63aec258f3655f586a5001f8e862970fb50
Jay <jaygallagher67@gmail.com>
2026-05-15 15:48:19 -0400

    CC tweek

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'node',
+  modulePathIgnorePatterns: [
+    '<rootDir>/github-pages'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/github-pages'
+  ]
+}
