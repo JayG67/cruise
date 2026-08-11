0698ea7adc0a0b661172e80fbf0735c70d7b5a2e
Jay <jaygallagher67@gmail.com>
2026-08-11 13:38:59 -0400

    Another fix for jest on GitHub

--- tests/integration/jest.integration.setup.js
+++ tests/integration/jest.integration.setup.js
@@ -1,8 +1,21 @@
 jest.setTimeout(120000)
 
 const db = require('../../db')
+const databaseCleanupTasks = []
+
+global.registerDatabaseCleanup = cleanup => {
+  if (typeof cleanup !== 'function') {
+    throw new TypeError('Database cleanup must be a function')
+  }
+
+  databaseCleanupTasks.push(cleanup)
+}
 
 afterAll(async () => {
+  for (const cleanup of databaseCleanupTasks) {
+    await cleanup()
+  }
+
   if (typeof db.closePool === 'function') {
     await db.closePool()
     return
